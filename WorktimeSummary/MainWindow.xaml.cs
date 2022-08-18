6ebed363526c701d4faac3480e7994c861ce6b6b
Melissa <melissa.menzel@twenty20.de>
2022-08-18 15:40:18 +0200

    hover-effect will happen even if hovered over the labels now

--- WorktimeSummary/MainWindow.xaml.cs
+++ WorktimeSummary/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿namespace WorktimeSummary
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
@@ -26,11 +28,13 @@
         public void AddRow(string[] values, bool isHeader = false)
         {
             DataGrid.RowDefinitions.Add(new RowDefinition());
-            DataGrid.RowDefinitions[DataGrid.RowDefinitions.Count - 1].MinHeight = isHeader ? 40 : 25;
+            RowDefinition row = DataGrid.RowDefinitions[DataGrid.RowDefinitions.Count - 1];
+            row.MinHeight = isHeader ? 40 : 25;
 
+            Border background = null;
             if (!isHeader)
             {
-                Border background = new Border
+                background = new Border
                 {
                     Background = DataGrid.RowDefinitions.Count % 2 == 0
                         ? DefaultRowBackgroundZebra1
@@ -57,19 +61,43 @@
 
                 lbl.HorizontalAlignment = HorizontalAlignment.Center;
                 lbl.VerticalAlignment = VerticalAlignment.Center;
+                lbl.Tag = background;
+                lbl.MouseEnter += LblOnMouseEnter;
+                lbl.MouseLeave += LblOnMouseLeave;
+                
                 Grid.SetColumn(lbl, i);
                 Grid.SetRow(lbl, DataGrid.RowDefinitions.Count - 1);
                 DataGrid.Children.Add(lbl);
             }
         }
 
+        private void LblOnMouseLeave(object sender, MouseEventArgs e)
+        {
+            BackgroundOnMouseLeave(((Label)sender).Tag, e);
+        }
+
+        private void LblOnMouseEnter(object sender, MouseEventArgs e)
+        {
+            BackgroundOnMouseEnter(((Label)sender).Tag, e);
+        }
+
         private void BackgroundOnMouseLeave(object sender, MouseEventArgs e)
         {
+            if (sender == null)
+            {
+                return;
+            }
+            
             ((Border)sender).Background = currentlyHoveredRowBackground;
         }
 
         private void BackgroundOnMouseEnter(object sender, MouseEventArgs e)
         {
+            if (sender == null)
+            {
+                return;
+            }
+            
             currentlyHoveredRowBackground = ((Border)sender).Background;
             ((Border)sender).Background = DefaultRowBackgroundHover;
         }
