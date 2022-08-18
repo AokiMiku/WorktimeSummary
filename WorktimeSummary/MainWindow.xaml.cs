080192926bb00099d32bcc95b03f7a7321af7bd8
Melissa <melissa.menzel@twenty20.de>
2022-08-18 15:02:31 +0200

    added some hover-effect for the rows

--- WorktimeSummary/MainWindow.xaml.cs
+++ WorktimeSummary/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 {
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Input;
     using System.Windows.Media;
     using controllers;
 
@@ -10,6 +11,12 @@
     /// </summary>
     public partial class MainWindow
     {
+        private static readonly Brush DefaultRowBackgroundZebra1 = Brushes.DarkGray;
+        private static readonly Brush DefaultRowBackgroundZebra2 = Brushes.DimGray;
+        private static readonly Brush DefaultRowBackgroundHover = Brushes.SlateGray;
+
+        private Brush currentlyHoveredRowBackground;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,20 +27,25 @@
         {
             DataGrid.RowDefinitions.Add(new RowDefinition());
             DataGrid.RowDefinitions[DataGrid.RowDefinitions.Count - 1].MinHeight = isHeader ? 40 : 25;
+
+            if (!isHeader)
+            {
+                Border background = new Border
+                {
+                    Background = DataGrid.RowDefinitions.Count % 2 == 0
+                        ? DefaultRowBackgroundZebra1
+                        : DefaultRowBackgroundZebra2
+                };
+                background.MouseEnter += BackgroundOnMouseEnter;
+                background.MouseLeave += BackgroundOnMouseLeave;
+                Grid.SetColumn(background, 0);
+                Grid.SetRow(background, DataGrid.RowDefinitions.Count - 1);
+                Grid.SetColumnSpan(background, DataGrid.ColumnDefinitions.Count);
+                DataGrid.Children.Add(background);
+            }
+
             for (int i = 0; i < 4; i++)
             {
-                if (!isHeader)
-                {
-                    Border background = new Border
-                    {
-                        Background = DataGrid.RowDefinitions.Count % 2 == 0 ? Brushes.LightGray : Brushes.DarkGray
-                    };
-                    Grid.SetColumn(background, i);
-                    Grid.SetRow(background, DataGrid.RowDefinitions.Count - 1);
-                    Grid.SetRowSpan(background, DataGrid.ColumnDefinitions.Count);
-                    DataGrid.Children.Add(background);
-                }
-
                 Label lbl = new Label
                 {
                     Content = values[i]
@@ -49,6 +61,17 @@
                 Grid.SetRow(lbl, DataGrid.RowDefinitions.Count - 1);
                 DataGrid.Children.Add(lbl);
             }
+        }
+
+        private void BackgroundOnMouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Border)sender).Background = currentlyHoveredRowBackground;
+        }
+
+        private void BackgroundOnMouseEnter(object sender, MouseEventArgs e)
+        {
+            currentlyHoveredRowBackground = ((Border)sender).Background;
+            ((Border)sender).Background = DefaultRowBackgroundHover;
         }
     }
 }
