eeccc92e38f0a7a0fb4279e968f683eca1ba94cf
Melissa <melissa.menzel@twenty20.de>
2022-08-18 15:06:43 +0200

    data folder was excluded from git; fixed it so that only the db is excluded

---
+++ WorktimeSummary/data/Worktimes.cs
@@ -0,0 +1,31 @@
+namespace WorktimeSummary.data
+{
+    using System;
+    using essentials;
+    using SQLite;
+
+    [Table("worktimes")]
+    public class Worktimes
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("day")] public string Day { get; set; }
+
+        [Column("pause")] public int Pause { get; set; }
+
+        [Column("worktime")] public double Worktime { get; set; }
+
+        [Column("is_workday")] public bool IsWorkday { get; set; }
+
+        [Column("starting_time")] public string StartingTimeString { get; set; }
+
+        public Time StartingTime
+        {
+            get => new Time(StartingTimeString);
+            set => StartingTimeString = value.ToString();
+        }
+    }
+}
---
+++ WorktimeSummary/data/WorktimesRepository.cs
@@ -0,0 +1,50 @@
+namespace WorktimeSummary.data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using SQLite;
+
+    public class WorktimesRepository
+    {
+        private readonly SQLiteAsyncConnection db =
+            new SQLiteAsyncConnection(Path.Combine(Environment.CurrentDirectory, "data", "wt.db"));
+
+        public WorktimesRepository()
+        {
+            db?.CreateTableAsync<Worktimes>();
+        }
+
+        public Worktimes FindById(int id)
+        {
+            AsyncTableQuery<Worktimes> q = db?.Table<Worktimes>().Where(w => w.Id == id);
+            Task<Worktimes> r = q?.ToListAsync().ContinueWith(w => w.Result.First());
+            try
+            {
+                return r?.Result;
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public List<Worktimes> FindAll()
+        {
+            AsyncTableQuery<Worktimes> q = db?.Table<Worktimes>();
+            Task<List<Worktimes>> r = q?.ToListAsync().ContinueWith(w => w.Result);
+            try
+            {
+                return r?.Result;
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
+}
